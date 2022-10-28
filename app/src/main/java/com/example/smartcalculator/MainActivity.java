742886f894a927085d9daac6437ab45f541f8716
Leyla <lelakarim@mail.ru>
2022-10-28 19:09:15 +0300

    menus done

--- app/src/main/java/com/example/smartcalculator/MainActivity.java
+++ app/src/main/java/com/example/smartcalculator/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.smartcalculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id=item.getItemId();
+        switch (id){
+            case R.id.menuSupport://как использовать калькуль
+                Toast.makeText(MainActivity.this,"It is not ready",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menuHelp://решение
+                Toast.makeText(MainActivity.this,"It is not ready too",Toast.LENGTH_LONG).show();
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
