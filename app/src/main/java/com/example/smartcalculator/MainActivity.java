391c7fe19475fb38db5661c876817aafd7b6ec26
Leyla <lelakarim@mail.ru>
2022-11-02 00:53:53 +0300

    fragments+-

--- app/src/main/java/com/example/smartcalculator/MainActivity.java
+++ app/src/main/java/com/example/smartcalculator/MainActivity.java
@@ -2,18 +2,78 @@ package com.example.smartcalculator;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.smartcalculator.Solution.FragmentNumbers;
+import com.example.smartcalculator.Solution.FragmentSigns;
+import com.example.smartcalculator.Solution.Solver.FragmentSolution;
+
 public class MainActivity extends AppCompatActivity {
+
+private final String TAG=this.getClass().getSimpleName();//для логов глобальная переменнная почему не статик
+
+FragmentSigns fragmentSigns=new FragmentSigns();
+FragmentNumbers fragmentNumbers=new FragmentNumbers();
+FragmentSolution fragmentSolution=new FragmentSolution();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button ButtonSigns=(Button)findViewById(R.id.buttonSigns);
+        Button ButtonNumbers=(Button)findViewById(R.id.buttonNumbers);
+        Button ButtonSolution=(Button)findViewById(R.id.buttonSolution);
+
+
+
+      setNewFragment(fragmentSigns);
+
+        ButtonSigns.setOnClickListener(this);
+        ButtonNumbers.setOnClickListener(this);
+        ButtonSolution.setOnClickListener(this);
+
+        View.OnClickListener onClickListener=new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.buttonSolution:
+                      setNewFragment(fragmentSolution);
+                        break;
+                    case R.id.buttonNumbers:
+                        setNewFragment(fragmentNumbers);
+                        Log.i(TAG,"fragmentNumbers");
+                        break;
+                    case R.id.buttonSigns:
+                        setNewFragment(fragmentSigns);
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + v.getId());
+                }
+            }
+        };
+
+
     }
+
+    private void setNewFragment(Fragment f) {
+        FragmentTransaction ft=getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragmentContainerView,f);
+        ft.commit();//это че сохраняет??
+    }
+
+
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main,menu);
         return true;
