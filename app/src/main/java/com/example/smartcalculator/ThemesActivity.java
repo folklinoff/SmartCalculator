693d2b42c3eab3f6ad0b79aeaf013e2fb22ecc8d
Leyla <lelakarim@mail.ru>
2022-11-17 00:25:40 +0300

    begin dinamic buttons, app works

--- app/src/main/java/com/example/smartcalculator/ThemesActivity.java
+++ app/src/main/java/com/example/smartcalculator/ThemesActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -11,13 +14,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.smartcalculator.Solution.TableThemes;
 import android.os.Bundle;
 
+import java.util.ArrayList;
 
-public class ThemesActivity extends AppCompatActivity {
+
+public class ThemesActivity extends AppCompatActivity implements View.OnClickListener {
 
     TableThemes tableThemes ;
 
-
-
+    LinearLayout linearLayout;
+    ArrayList<Button> arrayListOfButtonsThemes = new ArrayList<Button>();
+    Button ButtonThemeMultiplying;
+    Button ButtonThemeDivision;
+    Button ButtonThemeSubstraction;
 
 
     @Override
@@ -27,16 +35,35 @@ public class ThemesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_themes);
 
         tableThemes = new TableThemes (this);
-
         SQLiteDatabase base=tableThemes.getReadableDatabase();
 
+        linearLayout=(LinearLayout)findViewById(R.id.linearLayoutThemes);
 
+        arrayListOfButtonsThemes.add(ButtonThemeMultiplying);
+        arrayListOfButtonsThemes.add(ButtonThemeDivision);
+        arrayListOfButtonsThemes.add(ButtonThemeSubstraction);
+
+
+
+        ButtonThemeMultiplying.setOnClickListener(this);
+        ButtonThemeDivision.setOnClickListener(this);
+        ButtonThemeSubstraction.setOnClickListener(this);
 
     }
 
-   
+   public void addNewButton(){
+
+      if(arrayListOfButtonsThemes.get(arrayListOfButtonsThemes.size() - 1).getText()!="Write your theme"){
+
+          linearLayout.addView(R.id.);
+
+      }
+
+   }
 
 
+    @Override
+    public void onClick(View v) {
 
-
+    }
 }
