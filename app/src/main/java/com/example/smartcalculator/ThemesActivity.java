9629e92dd7dd29305955d78044b78be1eda6a084
Leyla <lelakarim@mail.ru>
2022-11-17 22:59:41 +0300

    begin dinamic buttons, app works

--- app/src/main/java/com/example/smartcalculator/ThemesActivity.java
+++ app/src/main/java/com/example/smartcalculator/ThemesActivity.java
@@ -1,9 +1,11 @@
 package com.example.smartcalculator;
 
+
+
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-
+import android.view.ViewGroup.LayoutParams;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -26,6 +28,8 @@ public class ThemesActivity extends AppCompatActivity implements View.OnClickLis
     Button ButtonThemeMultiplying;
     Button ButtonThemeDivision;
     Button ButtonThemeSubstraction;
+    Button ButtonTemp;
+
 
 
     @Override
@@ -39,6 +43,13 @@ public class ThemesActivity extends AppCompatActivity implements View.OnClickLis
 
         linearLayout=(LinearLayout)findViewById(R.id.linearLayoutThemes);
 
+
+        ButtonThemeSubstraction=(Button)findViewById(R.id.buttonThemeSubstraction);
+        ButtonThemeDivision=(Button)findViewById(R.id.buttonThemeDivision);
+        ButtonThemeMultiplying=(Button)findViewById(R.id.buttonThemeMultiplying);
+
+
+
         arrayListOfButtonsThemes.add(ButtonThemeMultiplying);
         arrayListOfButtonsThemes.add(ButtonThemeDivision);
         arrayListOfButtonsThemes.add(ButtonThemeSubstraction);
@@ -49,17 +60,35 @@ public class ThemesActivity extends AppCompatActivity implements View.OnClickLis
         ButtonThemeDivision.setOnClickListener(this);
         ButtonThemeSubstraction.setOnClickListener(this);
 
+
+
+
+
+       addNewButton();
     }
 
    public void addNewButton(){
 
       if(arrayListOfButtonsThemes.get(arrayListOfButtonsThemes.size() - 1).getText()!="Write your theme"){
 
-          linearLayout.addView(R.id.);
+          ButtonTemp=new Button(this);
+          ButtonTemp.setText("Write your theme");
+          LayoutParams Param = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+          ButtonTemp.setLayoutParams(Param);
+          linearLayout.addView(ButtonTemp);
+          ButtonTemp.setOnClickListener(this);
 
+          ButtonTemp.setOnLongClickListener(new View.OnLongClickListener() {
+              public boolean onLongClick(View v) {
+                  ButtonTemp.setText("opa!");
+                  return true;
+              }
+          });
       }
 
    }
+
+
 
 
     @Override
